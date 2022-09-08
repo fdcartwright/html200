b88cedba071084f9d8e381d7fd9fe66200c702a2
Dan Cartwright <f.d.cartwright@hotmail.com>
2022-09-08 13:47:25 -0700

    completed lesson4-challenge1

---
+++ lesson04/script.js
@@ -0,0 +1,13 @@
+
+function numberGame() {
+    let numberRequest = Number(prompt('Enter a number'));
+    alert(numberRequest * numberRequest);
+}
+
+function numberGame2() {
+    let numberRequest1 = Number(prompt('Enter a first number'));
+    let numberRequest2 = Number(prompt('Enter a second number'));
+    alert(numberRequest1 * numberRequest2);
+}
+
+
